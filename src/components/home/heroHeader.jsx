adcfa1516a6c1398f9bc5e066bff911b4837bd43
Abraham Samuel <ebubesammy567@gmail.com>
2025-03-07 03:45:58 +0100

    commit

--- src/components/home/heroHeader.jsx
+++ src/components/home/heroHeader.jsx
@@ -6,13 +6,13 @@ const HeroHeader = () => {
       const profilePic = images.find((image) => image.id === 7);
     
     return (
-      <div className="flex justify-between items-end w-full py-4 border-b">
+      <div className="flex flex-col gap-5 sm:flex-row lg:flex-row justify-between items-center sm:items-end  lg:items-end w-full py-4 border-b">
         <div className="">
           {profilePic && <img src={profilePic.img} alt="profile pics" />}
         </div>
 
         <div>
-          <Button>+ New Post</Button>
+          <Button className="w-[300px] lg:w-full sm:w-full">+ New Post</Button>
         </div>
       </div>
     );
